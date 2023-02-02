fac04efe3844c5b975b81c56f59105a1d90f514e
joumarhiba <joumhiba7@gmail.com>
2023-02-02 11:03:02 +0100

    initial commit

---
+++ mygov/ministereEF/src/main/java/com/mygov/MinistereEF.java
@@ -0,0 +1,11 @@
+package com.mygov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinistereEF {
+    public static void main(String[] args) {
+        SpringApplication.run(MinistereEF.class, args);
+    }
+}
