262f30ab9715cdd448366e879e51a68113b5afd7
joumarhiba <joumhiba7@gmail.com>
2023-02-06 22:29:58 +0100

    elasticsearch configured

--- mygov/ministereEF/src/main/java/com/mygov/Config/Config.java
+++ mygov/ministereEF/src/main/java/com/mygov/Config/Config.java
@@ -1,5 +1,7 @@
 package com.mygov.Config;
 
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,13 +13,18 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.mygov.Repository")
 @ComponentScan(basePackages = { "com.mygov.services" })
-public class Config {
-//    public RestHighLevelClient elasticsearchClient() {
-//        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-//                .connectedTo("localhost:9200")
-//                .build();
-//
-//        return RestClients.create(clientConfiguration)
-//                .rest();
-//    }
+public class Config extends AbstractElasticsearchConfiguration{
+
+    @Value("${elasticsearch.url}")
+    public String elasticsearchUrl;
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo(elasticsearchUrl)
+                .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+
 }
