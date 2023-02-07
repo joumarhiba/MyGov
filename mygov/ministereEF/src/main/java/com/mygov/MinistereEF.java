5ba0b95d4f80c0af946a0d3353233ec350750005
joumarhiba <joumhiba7@gmail.com>
2023-02-07 22:36:01 +0100

    some modifications

--- mygov/ministereEF/src/main/java/com/mygov/MinistereEF.java
+++ mygov/ministereEF/src/main/java/com/mygov/MinistereEF.java
@@ -3,9 +3,11 @@ package com.mygov;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableDiscoveryClient
 //@EnableElasticsearchRepositories(basePackages = "*")
 public class MinistereEF {
     public static void main(String[] args) {
--- mygov/ministereInterieur/src/main/java/com/mygov/MinistereInterieur.java
+++ mygov/ministereInterieur/src/main/java/com/mygov/MinistereInterieur.java
@@ -4,11 +4,14 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class MinistereInterieur {
 
     public static void main(String[] args) {
